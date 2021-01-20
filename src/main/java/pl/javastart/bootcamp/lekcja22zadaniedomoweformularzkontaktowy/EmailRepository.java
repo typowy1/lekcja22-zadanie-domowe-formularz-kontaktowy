a3b2433b1dcd95bef3a8eb9ad34598a1b1c424c0
Rafal Lesnicki <lesnickirafal@gmail.com>
2021-01-20 22:44:52 +0100

    added MailSender

--- src/main/java/pl/javastart/bootcamp/lekcja22zadaniedomoweformularzkontaktowy/EmailRepository.java
+++ src/main/java/pl/javastart/bootcamp/lekcja22zadaniedomoweformularzkontaktowy/EmailRepository.java
@@ -1,9 +1,5 @@
 package pl.javastart.bootcamp.lekcja22zadaniedomoweformularzkontaktowy;
 
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -21,22 +17,6 @@ public class EmailRepository {
 
     public void add(MyEmail email) {
         emails.add(email);
-    }
-
-    public void sendEmail(MyEmail myEmail) throws EmailException {
-        Email email = new SimpleEmail();
-        email.setHostName("smtp.gmail.com");
-        email.setSmtpPort(465);
-        email.setAuthenticator(new DefaultAuthenticator("testjavakurs", "javakurs1!"));
-        email.setSSLOnConnect(true);
-        email.setFrom(myEmail.getEmail(), myEmail.getName());
-        email.setSubject(myEmail.getTitle());
-        email.setMsg(myEmail.getMessage());
-        email.addTo("testjavakurs@gmail.com");
-        System.out.println("Rozpoczęto wysyłanie wiadomości email");
-        System.out.println("Dane wiadomości: " + myEmail.getEmail() + " " + myEmail.getName() + " " + myEmail.getTitle() + " " + myEmail.getMessage());
-        email.send();
-        System.out.println("Wiadomość wysłana");
     }
 
     public MyEmail getLastEmail() {
