327f160d03e9e9aaef444d7dd38a553f488106a6
Rafal Lesnicki <lesnickirafal@gmail.com>
2021-01-20 16:23:06 +0100

    Initial commit

---
+++ src/main/java/pl/javastart/bootcamp/lekcja22zadaniedomoweformularzkontaktowy/Lekcja22ZadanieDomoweFormularzKontaktowyApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.bootcamp.lekcja22zadaniedomoweformularzkontaktowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lekcja22ZadanieDomoweFormularzKontaktowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lekcja22ZadanieDomoweFormularzKontaktowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/bootcamp/lekcja22zadaniedomoweformularzkontaktowy/Lekcja22ZadanieDomoweFormularzKontaktowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.bootcamp.lekcja22zadaniedomoweformularzkontaktowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lekcja22ZadanieDomoweFormularzKontaktowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
